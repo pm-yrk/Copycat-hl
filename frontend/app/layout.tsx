f996dbea9d609abaf635a9ae257137f79460084f
Copycat-hl <paulmurrin13@gmail.com>
2026-06-28 20:22:54 +0100

    Compact dashboard card layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>{children}        <script defer src="/copycat_visual_layout_v1.js"></script>
               <script defer src="/copycat_visual_layout_v2.js"></script>
               <script defer src="/copycat_visual_layout_v3.js"></script>
+              <script defer src="/copycat_visual_layout_v4.js"></script>
       </body>
     </html>
   )
