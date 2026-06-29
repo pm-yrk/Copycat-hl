800d86c46715d60ec4aad87a1ed2b73bac435aef
Copycat-hl <paulmurrin13@gmail.com>
2026-06-29 22:45:47 +0100

    Compact disclaimer footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>{children}        <script defer src="/copycat_local_viewer_time.js"></script>
               <script src="/copycat_top_metrics_tight_asset_flush_v1.js" defer></script>
               <script src="/copycat_index_metric_rows_lift_v1.js" defer></script>
+              <script src="/copycat_compact_footer_v1.js" defer></script>
       </body>
     </html>
   )
