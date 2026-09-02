8ca4c71bf4ebd515bcef7de1c44cf8f9f0685970
Paul Murrin <paulmurrin13@gmail.com>
2026-09-02 11:38:45 +0100

    Clarify Telegram alert task cadence

--- scripts/local_snapshot_publisher/start_telegram_alerts_task.cmd
+++ scripts/local_snapshot_publisher/start_telegram_alerts_task.cmd
@@ -1,6 +1,6 @@
-@echo off
-cd /d "%~dp0"
-schtasks /Create /F /TN "CopycatTelegramAlerts" /SC MINUTE /MO 5 /TR "\"%~dp0run_telegram_alerts_once.cmd\""
-echo.
-echo Copycat Telegram alerts scheduled every 5 minutes.
-pause
+@echo off
+cd /d "%~dp0"
+schtasks /Create /F /TN "CopycatTelegramAlerts" /SC MINUTE /MO 5 /TR "\"%~dp0run_telegram_alerts_once.cmd\""
+echo.
+echo Copycat checks every 5 minutes and sends one hourly brief plus meaningful trigger alerts.
+pause
