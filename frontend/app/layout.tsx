8bfd7cabd5f570b0526bd8f923b3b890f75b8bb8
Paul Murrin <paulmurrin13@gmail.com>
2026-09-03 12:29:10 +0100

    Remove retired visual runtime patches

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,14 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" data-theme="dark">
-      <body>
-        <link rel="stylesheet" href="/copycat_mobile_source_layout_v3.css" />{children}        <script defer src="/copycat_local_viewer_time.js"></script>
-              <script src="/copycat_top_metrics_tight_asset_flush_v1.js" defer></script>
-<script src="/copycat_compact_footer_v1.js" defer></script>
-              <script src="/copycat_layer_footer_fix_v2.js" defer></script>
-              <script src="/copycat_restore_index_only_v1.js" defer></script>
-              <script src="/copycat_menu_layer_hotfix_v1.js" defer></script>
-            </body>
+      <head>
+        <link rel="stylesheet" href="/copycat_mobile_source_layout_v3.css" />
+      </head>
+      <body>{children}</body>
     </html>
   )
 }
